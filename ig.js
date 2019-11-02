f7a35d5edb845475265206472705a324a6f447cd
Yodsakorn009 <meanyok.mg@gmail.com>
2019-11-02 13:10:24 +0700

    v 1.0
    success code search

---
+++ ig.js
@@ -0,0 +1,81 @@
+
+$(function (){
+    // get instagram.json
+    $.get("https://www.instagram.com/explore/tags/555/?__a=1", function(data, status){
+        console.log(status);
+       
+        $("#location").text(data.graphql.hashtag.name);
+        //$("#slug").text(data.graphql.hashtag.slug);
+        $("#count").text(data.graphql.hashtag.edge_hashtag_to_media.count);
+
+        var imgUrl = data.graphql.hashtag.edge_hashtag_to_top_posts.edges[0].node.display_url;
+        // $("#post").attr("src", imgUrl); แบบไม่วนloop
+
+        for(node in data.graphql.hashtag.edge_hashtag_to_top_posts.edges){
+            var post = data.graphql.hashtag.edge_hashtag_to_top_posts.edges[node];
+            console.log(post.node.display_url);
+            var row =`<div class = "col-4">
+                            <img height = "300" width = "300" src="${post.node.display_url}" alt="">
+                            <div class = "row">
+                            <div class = "col">comments:${post.node.edge_media_to_comment.count}</div>
+                            <div class = "col">likes:${post.node.edge_liked_by.count}</div>
+                           
+                        </div>
+                        <div class="row">
+                        <div class = "col">caption:${post.node.edge_media_to_caption.edges[0].node.text}</div>
+                    </div>
+                </div>`;
+            $("#posts").append(row);
+        }
+
+    });
+
+
+    $("#search").click(function(){
+        var KeywordSeartch = $("#KeywordSeartch").val();
+        Igsearch(KeywordSeartch);
+        
+       });
+function  Igsearch(KeywordSeartch){
+    $("#posts").empty();
+
+    
+    // get instagram.json
+    $.get(`https://www.instagram.com/explore/tags/${KeywordSeartch}/?__a=1`, function(data, status){
+        console.log(status);
+ 
+        
+
+        
+        
+        $("#location").text(data.graphql.hashtag.name);
+        //$("#slug").text(data.graphql.hashtag.slug);
+        $("#count").text(data.graphql.hashtag.edge_hashtag_to_media.count);
+
+        var imgUrl = data.graphql.hashtag.edge_hashtag_to_top_posts.edges[0].node.display_url;
+        // $("#post").attr("src", imgUrl); แบบไม่วนloop
+
+        for(node in data.graphql.hashtag.edge_hashtag_to_top_posts.edges){
+            var post = data.graphql.hashtag.edge_hashtag_to_top_posts.edges[node];
+            console.log(post.node.display_url);
+            var row =`<div class = "col-4">
+                            <img height = "300" width = "300" src="${post.node.display_url}" alt="">
+                            <div class = "row">
+                            <div class = "col">comments:${post.node.edge_media_to_comment.count}</div>
+                            <div class = "col">likes:${post.node.edge_liked_by.count}</div>
+                           
+                        </div>
+                        <div class="row">
+                        <div class = "col">caption:${post.node.edge_media_to_caption.edges[0].node.text}</div>
+                    </div>
+                </div>`;
+            $("#posts").append(row);
+        }
+
+    });
+
+
+
+}
+
+});
